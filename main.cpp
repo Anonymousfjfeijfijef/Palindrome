93e592497534ff6f159ebcbd36c3ff9c7160df2f
U-BSD\481603 <481603@SHS-95W2NW3.beaverton.k12.or.us>
2026-09-14 11:14:33 -0700

    Finished Palindrome Checker

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+int main() {
+  char str[80];
+  char str2[80];
+  cin.get(str, 80);
+  cin.get(); 
+  int count = 0;
+  for (int i = 0; str[i] != '\0'; i++) {
+    if (isalpha(str[i])) {
+      str[i] = tolower(str[i]);
+      str2[count] = str[i];
+      count += 1;
+    }
+  }
+  str2[count] = '\0';
+  char str3[count + 1];
+  for (int i = 0; i < count; i++) {
+    str3[i] = str2[count-1-i];
+  }
+  str3[count] = '\0';
+  if (strcmp(str2, str3) == 0) {
+    cout << "Palindrome" << endl;
+  }
+  else {
+    cout << "Not a Palindrome" << endl;
+  }
+  return 0;
+}
